779c76015fc77951c741f3b6b9357c87faf3162d
Quang Hung Le <quanghungthegoat.2004@gmail.com>
2026-08-05 12:30:01 +1000

    modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 console.log('Ciao, Mondo!')
 console.log('Hallo, Welt!')
-console.log('Pair practical');
-console.log('Change made by Lok Mun');
-console.log(6 + 7)
 
 console.log(6 + 7)
-
-console.log("This part is made by Hung")
